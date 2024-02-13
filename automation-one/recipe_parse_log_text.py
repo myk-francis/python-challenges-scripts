d136b6b77c7288ea761410eb12fbb3a24840b11f
myk <michaelmugendi1707@gmail.com>
2024-02-13 10:55:16 +0300

    Update: split works too well

--- automation-one/recipe_parse_log_text.py
+++ automation-one/recipe_parse_log_text.py
@@ -1,10 +1,19 @@
 from delorean import Delorean
+from delorean import parse
 from decimal import Decimal
 
 d = Delorean()
 
 log = '[2018-05-05T11:07:12.267897] - SALE - PRODUCT: 1345 - PRICE: $09.99'
 
-devide_it = log.split('-')
-print(devide_it)
+divide_it = log.split('-')
+
+print(divide_it)
+
+timestamp_string = divide_it[0]
+product_id = divide_it[2]
+price = divide_it[3]
+
+# timestamp_string, _, product_string, price_string = divide_it
+print(timestamp_string, product_id, price)
 
