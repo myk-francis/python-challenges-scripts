9a6bfc6288fae9342d5c0c0e27dd50cc815bafb1
myk <michaelmugendi1707@gmail.com>
2024-02-13 12:34:18 +0300

    Update: fixed format bug

--- automation-one/recipe_parse_log_text_1.py
+++ automation-one/recipe_parse_log_text_1.py
@@ -2,7 +2,9 @@ from parse import *
 
 LOG = '[2018-05-06T12:58:00.714611] - SALE - PRODUCT: 1345 - PRICE: $09.99'
 
-FORMAT = '[{date}] - SALE - PRODUCT: {product} - PRICE:${price}'
+
+# FORMAT = '[{date}] - SALE - PRODUCT: {product} - PRICE: ${price}'
+FORMAT = '[{date:ti}] - SALE - PRODUCT: {product:d} - PRICE: ${price:05.2f}'
 
 result = parse(FORMAT, LOG)
 
