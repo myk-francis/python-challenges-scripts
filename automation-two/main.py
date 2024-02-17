e6cdc711f23166027333570570d8aa5a9bcf0bee
myk <michaelmugendi1707@gmail.com>
2024-02-18 00:08:16 +0300

    Update: added messaging

--- automation-two/main.py
+++ automation-two/main.py
@@ -1,11 +1,19 @@
 
 from jobs.jobs import Jobs
+import pywhatkit
+
 
 with Jobs() as bot:
+    final_text = ""
     bot.landing_page()
     titles = bot.get_titles()
     dates = bot.get_closing_dates()
     print(f'There are {len(titles)} jobs:')
     for title, date in zip(titles, dates):
-        print(f'{title} - {date}')
+        final_text += f'{title} - {date}\n'
+
+    print(final_text)
+
+    pywhatkit.sendwhatmsg("+255710503304", final_text, 0, 4, True, 2)
+    
     print('Done!')
