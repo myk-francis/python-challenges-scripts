fbd55a004130fbacc59301768b06d7dbaf7f0fcf
myk <michaelmugendi1707@gmail.com>
2024-02-17 22:45:57 +0300

    Update: added wait and maximized window

--- automation-two/jobs/jobs.py
+++ automation-two/jobs/jobs.py
@@ -1,12 +1,14 @@
-
+from selenium import webdriver
 import jobs.constants as const 
 
-class Jobs:
-    def __init__(self, driver):
-        self.driver = driver
+class Jobs(webdriver.Chrome):
+    def __init__(self):
+        super(Jobs, self).__init__()
+        self.implicitly_wait(10)
+        self.maximize_window()
 
-    def __exit__(self):
-        self.driver.quit()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.quit()
 
     def landing_page(self):
-        self.driver.get(const.BASE_URL)
+        self.get(const.BASE_URL)
--- automation-two/main.py
+++ automation-two/main.py
@@ -1,8 +1,6 @@
-from selenium import webdriver
+
 from jobs.jobs import Jobs
 
-browser = webdriver.Chrome()
-
-with Jobs(driver=browser) as bot:
+with Jobs() as bot:
     bot.landing_page()
     print('Done!')
